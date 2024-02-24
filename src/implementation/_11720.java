810b3b22600a292d57defda815d375c96da571ee
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-02-24 17:26:18 +0900

    solve

--- src/implementation/_11720.java
+++ src/implementation/_11720.java
@@ -1,15 +1,19 @@
 package implementation;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _11720 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();//숫자 개수 입력받기
-        String str=sc.next();//일단 문자열로 받기
-        for(int i=0;i<str.length();i++){
-
-
+        String str=sc.next();
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum+=str.charAt(i)-'0';// 범위가 변수>리터럴인경우 자동 형변환 가능.
         }
+        System.out.println(sum);
+
+
     }
 }
