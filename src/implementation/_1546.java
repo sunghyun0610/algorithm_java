4cf720957530d6d3c9065d9c08b00a710d550665
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-02-26 14:58:05 +0900

    bufferedReader사용해보기

--- src/implementation/_1546.java
+++ src/implementation/_1546.java
@@ -1,16 +1,21 @@
 package implementation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class _1546 {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        int N= sc.nextInt();//시험본 과목 개수 입력
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int N= Integer.parseInt(br.readLine());//시험본 과목 개수 입력
         double [] score=new double[N];
         double max=0;
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
         for(int i=0;i<N;i++){
-            score[i]=sc.nextInt();
+            score[i]=Double.parseDouble(st.nextToken());
             if(max<score[i])
                 max=score[i];// 최대값 확인
         }
