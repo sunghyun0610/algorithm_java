0466314fa8ba289863d6f370d80e20dc1854ee4f
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-02-28 00:37:18 +0900

    투포인터 알고리즘

--- src/implementation/_2018.java
+++ src/implementation/_2018.java
@@ -4,30 +4,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class _2018 {
-    public static void main(String[] args)throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int N= Integer.parseInt(br.readLine());//숫자가 주어짐.
-        ArrayList<Long> sum= new ArrayList<>();
-        long tmp=0;
-        for(int i=1;i<N/2+2;i++){
-            tmp+=i;
-            sum.add(tmp);
-
-            System.out.println(sum);
-        }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int N=sc.nextInt();
         int cnt=1;
-        for(int j=0;j<sum.size();j++){
+        int start_index=1;
+        int end_index=1;
+        int sum=1;
+        while(end_index!=N){
+            if (sum == N) {
+                cnt++; end_index++; sum+=end_index;
+            }
+            else if(sum>N){
+                sum-=start_index;
+                start_index++;
 
-            if(N==sum.get(j))
-                cnt++;
-            for(int k=0;k<sum.size();k++){
-                if(sum.get(k)- sum.get(j)==N)
-                    cnt++;
+            }
+            else{
+                end_index++; sum+=end_index;
             }
         }
-
         System.out.println(cnt);
     }
 }
+
+
+/*
+* import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer = 0;
+
+        for(int i=1; i<=n; i++){
+            int sum = 0;
+            for(int j=i; j<=n; j++){
+                sum+=j;
+                if(sum >n) break;
+                else if(sum == n){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
+* */
