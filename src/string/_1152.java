2adb64789ffcbeb5f35e2b2c6383aa626f8d9048
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-02 14:53:08 +0900

    split함수 이용

---
+++ src/string/_1152.java
@@ -0,0 +1,28 @@
+package string;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class _1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();//String은 배열이 아니므로 인덱스로 바로 접근 불가
+        int cnt = 0;
+        String[] list = str.trim().split(" ");
+
+        if(str.trim().isEmpty()){
+            cnt=0;
+            // 입력된 문자열이 공백만 있을 경우 단어 수는 0
+        }
+        else {
+            // 공백으로 분리된 단어의 수 계산
+            cnt=list.length;
+        }
+        System.out.println(cnt);
+    }
+}
+//공백은 단어로 취급 X 오직 단어를 구분하는 역할
