ca22688bfef5848bef6da1acb2bd16c970b43b84
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-05 18:57:31 +0900

    투 포인터

--- src/implementation/_1940.java
+++ src/implementation/_1940.java
@@ -1,27 +1,37 @@
 package implementation;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _1940 {
-    public static void main(String [] args){
-        Scanner sc= new Scanner(System.in);
-        int N=sc.nextInt();//재료 개수
-        int M=sc.nextInt();//갑옷 만드는데 필요한 수
-        int [] num=new int[N];
-        for(int i=0;i<N;i++){
-            num[i]=sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();//재료 개수
+        int M = sc.nextInt();//갑옷 만드는데 필요한 수
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
 
         }
-        int sum=0;
+        Arrays.sort(num);//오름차순 정렬!
+        int start_index=0;
+        int end_index=N-1;
         int cnt=0;
-        for(int i=0;i<N;i++){
-            for(int j=i+1;j<N;j++){
-                sum=num[i]+num[j];
-                if(sum==M){
-                    cnt++;
-                }
+        int sum=0;
+        while(start_index<end_index){
+            if(num[start_index] + num[end_index]==M){
+                cnt++; start_index++; end_index--;
+            }
+            else if(num[start_index] + num[end_index]>M){
+                end_index--;
+            }
+            else {
+                start_index++;
             }
         }
+
         System.out.println(cnt);
     }
 }
+
+//오름차순 정렬하고 풀면 쉽다!
