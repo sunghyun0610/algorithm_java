08de3234894ddf0b3080f7e047535bf927891e9b
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-11 14:24:19 +0900

    solved

--- src/two_pointer/_12891.java
+++ src/two_pointer/_12891.java
@@ -1,4 +1,52 @@
 package two_pointer;
+import java.util.Scanner;
 
+// 슬라이딩 윈도우 : 범위가 일정한 투 포인터라고 생각하면 됨.
 public class _12891 {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int S=sc.nextInt();//문자열 길이 A,C,G,T만 사용가능
+        int P=sc.nextInt();//부분 문자열 길이
+        String str=sc.next();
+        int [] dna=new int[4];
+        for(int i=0;i<4;i++){
+            dna[i]=sc.nextInt();
+        }
+        int cnt=0;
+        int start_index=0;
+        int [] count= new int[4];
+        for(int i=0;i<P;i++){//처음 슬라이딩 윈도우
+            if(str.charAt(i)=='A')
+                count[0]++;
+            else if(str.charAt(i)=='C')
+                count[1]++;
+
+            else if(str.charAt(i)=='G')
+                count[2]++;
+            else if (str.charAt(i)=='T')
+                count[3]++;
+        }
+        if(count[0]>=dna[0] && count[1]>=dna[1] && count[2]>=dna[2] && count[3]>=dna[3] ) cnt++;
+
+        for(int j=P;j<S;j++){
+            int pre_index=j-P;
+            if(str.charAt(pre_index)=='A') count[0]--;
+            else if(str.charAt(pre_index)=='C') count[1]--;
+            else if(str.charAt(pre_index)=='G') count[2]--;
+            else if(str.charAt(pre_index)=='T') count[3]--;
+
+            if(str.charAt(j)=='A') count[0]++;
+
+            else if(str.charAt(j)=='C') count[1]++;
+
+            else if(str.charAt(j)=='G') count[2]++;
+
+            else if(str.charAt(j)=='T') count[3]++;
+
+            if(count[0]>=dna[0] && count[1]>=dna[1] && count[2]>=dna[2] && count[3]>=dna[3] ) cnt++;
+        }
+
+
+        System.out.println(cnt);
+    }
 }
