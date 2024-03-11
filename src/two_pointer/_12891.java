f03b3abb01f622c17b5bfa56e2dd73f0a69209e0
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-11 17:21:20 +0900

    슬라이딩 윈도우 알고리즘

--- src/two_pointer/_12891.java
+++ src/two_pointer/_12891.java
@@ -1,8 +1,9 @@
 package two_pointer;
 import java.util.Scanner;
 
-// 슬라이딩 윈도우 : 범위가 일정한 투 포인터라고 생각하면 됨.
+// 슬라이딩 윈도우 : '범위'가 일정한 투 포인터라고 생각하면 됨.
 public class _12891 {
+    //P와 S의 길이가 1000000백만으로 크기때문에 시간복잡도 고려해야함. O(N)으로 풀어야한다는 결론
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         int S=sc.nextInt();//문자열 길이 A,C,G,T만 사용가능
@@ -15,7 +16,7 @@ public class _12891 {
         int cnt=0;
         int start_index=0;
         int [] count= new int[4];
-        for(int i=0;i<P;i++){//처음 슬라이딩 윈도우
+        for(int i=0;i<P;i++){//처음 슬라이딩 윈도우 만들어 놓고 앞뒤로 하나씩만 줄이고 늘려가면서 진행한다.
             if(str.charAt(i)=='A')
                 count[0]++;
             else if(str.charAt(i)=='C')
