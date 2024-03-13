6d4dd4900c559f1ed57bae8b300e6058fb5794cf
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-13 14:15:08 +0900

    dd

--- src/implementation/_11720.java
+++ src/implementation/_11720.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class _11720 {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();//숫자 개수 입력받기
-        String str=sc.next();
-        int sum=0;
-        for(int i=0;i<N;i++){
-            sum+=str.charAt(i)-'0';// 범위가 변수>리터럴인경우 자동 형변환 가능.
+            Scanner sc=new Scanner(System.in);
+            int N=sc.nextInt();//숫자 개수 입력받기
+            String str=sc.next();//최대 100자리 숫자이므로 int 사용 불가능 String
+            int sum=0;
+            for(int i=0;i<N;i++){
+                sum+=str.charAt(i)-'0';// 범위가 변수>리터럴인경우 자동 형변환 가능.
         }
         System.out.println(sum);
     }
