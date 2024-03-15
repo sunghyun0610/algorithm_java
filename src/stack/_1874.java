1c45a5efbed65c8af86ba84c53d8d0cd8efb6191
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-15 23:28:54 +0900

    1874

--- src/stack/_1874.java
+++ src/stack/_1874.java
@@ -1,5 +1,25 @@
 package stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class _1874 {
+    public static void main(String []args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        int n= Integer.parseInt(st.nextToken());
+        int [] num=new int[n];
+        for (int i=0;i<n;i++){
+            num[i]=Integer.parseInt(br.readLine());
+        }
+
+        Stack<Integer> stack=new Stack<>();
+
+
+
+    }
 
 }
