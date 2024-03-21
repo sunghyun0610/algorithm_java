a2775bf41c3480cfa8bed9e6ac6ad15669517073
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-21 14:17:30 +0900

    solved

--- src/string/_2164.java
+++ src/string/_2164.java
@@ -8,8 +8,19 @@ public class _2164 {
     public static void main(String [] args){
         Scanner sc= new Scanner(System.in);
         int N=sc.nextInt();
-        //제일 위에서 하나빼고=dequeue(앞에서 삭제), 제일 뒤로 보내기=enqueue(뒤에 삽입)
+        //제일 위에서 하나빼고=dequeue(앞에서 삭제), 제일 뒤로 보내기=enqueue(뒤에 삽입) : 즉, 큐 자료구조를 이해하냐는 질문
         Queue<Integer> queue= new LinkedList<>();
+        for(int i=0;i<N;i++){
+            queue.add(i+1);
+        }
+        while(queue.size()>1){
+            queue.poll();//맨위의 값 반환 후 삭제
+            int num=queue.peek();
+            queue.poll();
+            queue.add(num);
+        }
+        int result=queue.peek();
+        System.out.println(result);
     }
 
 }
