88d9772ac34404b0bf8660e48507bff7b24aa584
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-21 13:50:36 +0900

    queue

---
+++ src/string/_2164.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class _2164 {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        int N=sc.nextInt();
+        //제일 위에서 하나빼고=dequeue(앞에서 삭제), 제일 뒤로 보내기=enqueue(뒤에 삽입)
+        Queue<Integer> queue= new LinkedList<>();
+    }
+
+}
