555c3253eddc320978ddf3260318153b5a7432f5
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-30 23:48:20 +0900

    BaekJoon_1427 ing(문자 배열에 저장까지 구현)

--- src/sorting/_1427.java
+++ src/sorting/_1427.java
@@ -13,6 +13,7 @@ public class _1427 {
         int []num=new int[str.length()];
         for (int i=0;i<str.length();i++){
             num[i]=Character.getNumericValue(str.charAt(i));// Character,getNumericValue : 단일문자 정수 변환
+
         }
 
     }
