ad3a3f28d99618b5611a30c0a4a3e19ac9761461
sunghyun <20190171@inu.ac.kr>
2024-08-09 17:00:39 +0900

    feat : dfs/bfs 구현연습

--- src/graph/_1012.java
+++ src/graph/_1012.java
@@ -1,26 +1,96 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-class Graph{
-    class Node{
-        int data;
-        LinkedList<Node> adjacent;
-        boolean marked;
-        Node(int data){
-            this.data =data;
-            this. marked =false;
-            adjacent = new LinkedList<>();
+public class _1012 {
+
+    static int N;
+    static int M;
+    static int K;
+    static int [][] farm;
+    static boolean [][] visited;
+    static int [] di={-1,1,0,0};//상하
+    static int [] dj={0,0,-1,1};//좌우
+    static int cnt;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int T= Integer.parseInt(st.nextToken());//test case 개수
+        for(int c=0;c< T;c++){
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());//가로
+            N = Integer.parseInt(st.nextToken());//세로
+            K = Integer.parseInt(st.nextToken());//배추의 개수
+            farm = new int[N][M];//가로 세로 와 행렬의 행 렬 개념을 잘 동기화해봐
+            visited = new boolean[N][M];
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());//x좌표 입력
+                int y = Integer.parseInt(st.nextToken());//y좌표 입력
+                farm[y][x] = 1;//1로 증가//행렬의 x y 좌표는 반대임 이건 그림 그려보면서 해야댐.
+            }
+            cnt = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (farm[i][j] == 1 && !visited[i][j]) {//1인 곳만 전체를 돌면서 체크를 하면되니까
+                        bfs(i, j);
+                        cnt++;//이 흐름의 묶음의 개수를 구하는거니까 여기서 cnt하는구나
+                    }
+                }
+            }
+            System.out.println(cnt);
         }
     }
 
-}
 
-public class _1012 {
-    public static void main(String[] args) {
+    static void dfs(int i, int j){
+        if(farm[i][j]==1 && !visited[i][j]) {
+            visited[i][j] = true;//나 여기 서 있음! 방문했음!{
+                    for (int k = 0; k < 4; k++) {
+                        int nexti= i+di[k];
+                        int nextj= j+dj[k];
+                        if(nexti>=0&&nexti<N && nextj>=0&&nextj <M && farm[nexti][nextj]==1&&!visited[nexti][nextj]){
+                            dfs(nexti,nextj);
+
+                    }
+                }
+            }
+        }
+
+        static void bfs(int i,int j){//1인 좌표들만 오는 중
+            visited[i][j]=true;
+            Queue<Point> bfs_queue=new LinkedList<>();
+            bfs_queue.add(new Point(i,j));
+            while(!bfs_queue.isEmpty()){
+                Point now= bfs_queue.poll();
+                for(int k=0;k<4;k++){
+                    int nexti=now.i+di[k];//세로 인덱스
+                    int nextj=now.j+dj[k];//가로 인덱스 - M
+                    if(nexti>=0&&nexti<N && nextj>=0 && nextj<M && farm[nexti][nextj]==1&&!visited[nexti][nextj]){
+                        bfs_queue.add(new Point(nexti,nextj));
+                        visited[nexti][nextj]=true;
+                    }
+                }
+            }
+
+        }
+
+       static class Point{
+            int i,j;
+            Point(int i,int j){
+                this.i=i;
+                this.j=j;
+            }
+        }
 
     }
-}
 
 /*
 * 이게왜 그래프로 풀리는 문제인지 알아보는 시간~
