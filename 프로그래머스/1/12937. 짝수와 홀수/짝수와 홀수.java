ff3fd9aecba78851867c3686ddeaf3d7db6ef0fd
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2025-01-07 19:19:00 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,22 +1,13 @@
 class Solution {
     public String solution(int num) {
         String answer = "";
-        if(num==0 || num%2==0){
-            answer = "Even";
+        if(num%2==0){
+            answer="Even";
         }
-        else if(num%2==1){
-            answer = "Odd";
+        else{
+            answer="Odd";
         }
-        else if(num<0){
-            num*=-1;
-            if(num==0 || num%2==0){
-            answer = "Even";
-        }
-        else if(num%2==1){
-            answer = "Odd";
-        }
-            
-        }
+        
         return answer;
     }
 }
