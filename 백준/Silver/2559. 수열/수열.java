93c366ad15870e4d58d852b4c047dad4e85dd7aa
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2025-02-07 17:56:25 +0900

    [Silver III] Title: 수열, Time: 688 ms, Memory: 109548 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -7,31 +7,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();//전체 날짜 수
         int K = sc.nextInt();//합을 구하기위한 연속적인 날짜
-        int maxTem= Integer.MIN_VALUE;
+        int maxTem =0;
 
         int[] tem = new int[N];
         for(int i=0;i<N;i++){
             tem[i] = sc.nextInt();
         }//온도 입력받기 완료
-
+        for(int i=0; i<K;i++){
+            maxTem += tem[i];
+        }
+        int last=maxTem;
         for(int i=0;i<N-K;i++){
-            if(tem[i]<tem[i+K]) {
-                continue;
-            }
-            int tmp=0;
-            for (int j = 0; j < K; j++) {
-                tmp += tem[i + j];
-            }
-            if(tmp>maxTem) maxTem=tmp;
+          int tmp = last-tem[i] +tem[i+K];
+          last=tmp;
+
+          if(tmp>maxTem) maxTem=tmp;
         }
-        int last=0;
-        for(int i=N-1;i>=N-K;i--){
-            last += tem[i];
-        }
-        if(last>maxTem) maxTem = last;
         System.out.println(maxTem);
 
 
     }
 }//연속적으로 며칠동안 온도의 합이 가장 큰 값을 계산하는 프로그램
 //어차피 몇개를 해도 처음 마지막만 차이나는거임
+//부분순열 크기 고정 -> 슬라이딩 윈도우
