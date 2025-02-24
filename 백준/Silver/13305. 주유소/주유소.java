3267093feb8f47b7ef7a18c3501394ecfedfaf6c
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2025-02-24 12:54:39 +0900

    [Silver III] Title: 주유소, Time: 840 ms, Memory: 169144 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] len = new int[N-1];//도시사이 거리
+        int[] price = new int[N];//도시별 리터당 기름 값
+
+        for(int i=0;i<N-1;i++){
+            len[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<N;i++){
+            price[i] = sc.nextInt();
+        }
+
+        int answer=0;
+        answer+= price[0]*len[0];
+
+        int i=1;
+        while(i<N-1){
+            int tmp=0;
+            int tmpPrice=price[i];
+            while((i+tmp)<N-1 && tmpPrice<=price[i+tmp]){
+                answer+=tmpPrice*len[i+tmp];
+                tmp++;
+            }
+            i+=tmp;
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
+//기름통의 크기는 무제한
+//1km 이동 == 1리터 기름
+//각 도시에 1나의 주유소, 각 도시마다 가격 다름
+//2번째 부터 그 이후로 나보다 싼 가격이 나올때 까지 다넣어버림
