d82170d1056622f7dcea7bb84ec177298b41db73
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2025-03-11 17:25:52 +0900

    [level 2] Title: 올바른 괄호, Time: 16.78 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,35 +2,22 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Queue<Character> queue = new LinkedList<>();
+
+        Stack<Character> stack = new Stack<>();
+        
         for(int i=0;i<s.length();i++){
-            queue.add(s.charAt(i));
-        }//queue에 모두 집어넣기 완료
-        int cnt1=0;
-        int cnt2=0;
-        
-        while(!queue.isEmpty()){
-            char ch = queue.poll();
+            char ch = s.charAt(i);
             if(ch=='('){
-                cnt1++;
+                stack.push('(');
+                
             }
-            else if(ch==')'){
-                cnt2++;
-            }
-            if(cnt1<cnt2){
-                answer=false;
-                break;
+            else{
+                if(stack.isEmpty()) return false;
+                stack.pop();
             }
         }
-        if(cnt1==cnt2) answer=true;
-        else{
-            answer=false;
-        }
-        
+        if(stack.size()>0) return false;
 
         return answer;
     }
 }
-//열렸으면 닫혀야함
-//먼저 닫히고 열리는거 없음
-//문자하나하나 큐에 넣고 ( 가나올때마다 count, )도 카운트하면서 )가 (보다많아지면 false
