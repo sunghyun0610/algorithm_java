106f2a0e907f94d61a65c27bc5531e3183433972
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-07-23 11:50:52 +0900

    [level 3] Title: N으로 표현, Time: 13.20 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,44 +2,41 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         int answer = 0;
-        List<Set<Integer>> numList = new ArrayList<>();
-    
-        for(int i=0;i<=8;i++){
-            numList.add(new HashSet<>());
+        
+        String[] calculateType ={"plus","minus","multiple","divide","join"};
+        Set<Integer>[] resultSet = new HashSet[9]; //모든 결과값을 set에 추가해서 원하는 타겟 포함되면 종료
+        for(int i=0;i<9;i++){
+            resultSet[i] = new HashSet<>();
         }
-        numList.get(1).add(N);// 1개 사용해서 만들 수 있는건 자기자신 뿐
-        if(numList.get(1).contains(number)) return 1;
-        for(int i=2;i<=8;i++){
-            StringBuilder sb = new StringBuilder();
-            for(int l=0;l<i;l++){
-                sb.append(N);
-            }
-            int repeat = Integer.parseInt(sb.toString());
+        
+        int joinedNumber = 0;
+        
+        for(int cnt=1;cnt<=8;cnt++){
+            joinedNumber = joinedNumber*10+N;
+            resultSet[cnt].add(joinedNumber);
             
-            numList.get(i).add(repeat);
-            for(int j=1;j<i;j++){
-                for(int k=1;k<i;k++){
-                    if(j+k==i){
-                        for(Integer a : numList.get(j)){
-                            for(Integer b : numList.get(k)){
-                                numList.get(i).add(a+b);
-                                numList.get(i).add(a-b);
-                                if(b!=0){
-                                    numList.get(i).add(a/b);
-                                }
-                                numList.get(i).add(a*b);
-                            }
-                        }
-                    }
-                    if(numList.get(i).contains(number)){
-                        return i;
+            for(int left=1; left<cnt; left++){
+                int right = cnt-left;
+                
+                for(int a : resultSet[left]){
+                    for(int b: resultSet[right]){
+                        resultSet[cnt].add(a+b);
+                        resultSet[cnt].add(a-b);
+                        resultSet[cnt].add(a*b);
+                        if(b==0) continue;
+                        resultSet[cnt].add(a/b);
                     }
                 }
             }
+            if(resultSet[cnt].contains(number)) return cnt;
         }
         
-        
-        answer=-1;
-        return answer;
+        return -1;
     }
+   
 }
+// 경우의수
+// 숫자 붙여쓰는경우
+// 더하는경우, 뺴는경우, 나누는경우, 곱하는경우
+// 연산결과를 다 저장해볼까?
+// N1개써서 만들수있는값들.. N2개써서 만들수있는 값들..
