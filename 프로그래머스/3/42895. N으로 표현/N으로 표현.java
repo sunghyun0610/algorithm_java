220be700b3a0fe581f97ba74136f971959b6f379
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-07-24 10:01:58 +0900

    [level 3] Title: N으로 표현, Time: 16.58 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,41 +2,38 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         int answer = 0;
-        
-        String[] calculateType ={"plus","minus","multiple","divide","join"};
-        Set<Integer>[] resultSet = new HashSet[9]; //모든 결과값을 set에 추가해서 원하는 타겟 포함되면 종료
+        int cnt=0;
+        Set<Integer>[] resultSet = new HashSet[9];
         for(int i=0;i<9;i++){
             resultSet[i] = new HashSet<>();
-        }
-        
-        int joinedNumber = 0;
-        
-        for(int cnt=1;cnt<=8;cnt++){
-            joinedNumber = joinedNumber*10+N;
-            resultSet[cnt].add(joinedNumber);
+        }//dp배열 초기화 완료
+        resultSet[1].add(N);
+        cnt++;
+        while(cnt<=8){
+            //cnt-1 개수만큼은 저장된 상태임
+            //연속된 숫자 계산
+            String joinNum ="";
+            for(int i=0;i<cnt;i++){
+                joinNum +=String.valueOf(N); 
+            }
+            int joinNumber = Integer.parseInt(joinNum);
+            resultSet[cnt].add(joinNumber);
             
             for(int left=1; left<cnt; left++){
                 int right = cnt-left;
-                
-                for(int a : resultSet[left]){
+                for(int a :resultSet[left]){
                     for(int b: resultSet[right]){
                         resultSet[cnt].add(a+b);
                         resultSet[cnt].add(a-b);
                         resultSet[cnt].add(a*b);
-                        if(b==0) continue;
-                        resultSet[cnt].add(a/b);
+                        if(b!=0) resultSet[cnt].add(a/b);
                     }
                 }
             }
             if(resultSet[cnt].contains(number)) return cnt;
+            cnt++;
         }
         
         return -1;
     }
-   
 }
-// 경우의수
-// 숫자 붙여쓰는경우
-// 더하는경우, 뺴는경우, 나누는경우, 곱하는경우
-// 연산결과를 다 저장해볼까?
-// N1개써서 만들수있는값들.. N2개써서 만들수있는 값들..
