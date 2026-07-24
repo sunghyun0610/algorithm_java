2443bba6ac16325f263f63618f4a1ac93eab645f
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-07-24 11:10:04 +0900

    [level 2] Title: 땅따먹기, Time: 15.06 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,31 +1,40 @@
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
-        int row = land.length;
-        
-        int[][] DP = new int[row][4];
-        for(int i=0;i<4;i++){
-            DP[0][i]=land[0][i];
+
+        int row = land.length; //행 개수
+        int cnt=0; //지금 몇행인지
+        int [][] dp = new int[row][4];
+        for(int i=0;i<row;i++){
+            if(i==0){
+                for(int j=0;j<4;j++){
+                    dp[i][j]=land[i][j];
+                }
+            }
+            else{//1행 이상일 때
+                for(int j=0;j<4;j++){
+                    if(j==0){
+                        dp[i][j] = land[i][j]+( Math.max(dp[i-1][1],Math.max(dp[i-1][2],dp[i-1][3])));
+                    }
+                    if(j==1){
+                        dp[i][j] = land[i][j]+( Math.max(dp[i-1][0],Math.max(dp[i-1][2],dp[i-1][3])));
+                    }
+                    if(j==2){
+                        dp[i][j] =land[i][j]+ (Math.max(dp[i-1][1],Math.max(dp[i-1][0],dp[i-1][3])));
+                    }
+                    if(j==3){
+                        dp[i][j] = land[i][j]+( Math.max(dp[i-1][1],Math.max(dp[i-1][2],dp[i-1][0])));
+                    }
+                }
+            }
+            
+        }
+        for(int K : dp[row-1]){
+            // System.out.println("K : "+K);
+            answer = Math.max(K,answer);
         }
         
-        for(int i=1;i<row;i++){
-            land[i][0]+=Math.max(Math.max(land[i-1][1],land[i-1][2]),land[i-1][3]);
-            land[i][1]+=Math.max(Math.max(land[i-1][0],land[i-1][2]),land[i-1][3]);
-            land[i][2]+=Math.max(Math.max(land[i-1][1],land[i-1][0]),land[i-1][3]);
-            land[i][3]+=Math.max(Math.max(land[i-1][1],land[i-1][2]),land[i-1][0]);
-        }
-        
-        // for(int k : DP[3]){
-        //     System.out.println(k);
-        // }
-        
-        for(int i=0;i<4;i++){
-            answer = Math.max(answer, land[row-1][i]);
-        }
 
         return answer;
     }
 }
-//dfs는 O(4^N) 이므로 시간복잡도 초과한다
-//항상 최대값 고르는게 아니네. 선택이 그 다음 선택에 영향을 주니까
-//지금 작은거 골라도 다음 합까지한 결과값이 더 크다면 작은거 골라야함
