818e3d3dc5e874218652aa72cfa1e6d133fae8e7
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-08-02 11:19:31 +0900

    [level 3] Title: 불량 사용자, Time: 24.04 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,66 +1,54 @@
-import java.util.Map;
-import java.util.HashMap;
 import java.util.*;
 
 class Solution {
     static Set<List<String>> resultSet;
-    static boolean[] visited;
+    static boolean[] used;
     public int solution(String[] user_id, String[] banned_id) {
         int answer = 0;
-        Map<String, String[]> bannedIdMatchedMap = new HashMap<>();
-        resultSet = new HashSet<>();//이 set사이즈가 결국 answer이 되겠네
-        visited = new boolean[user_id.length];
-        List<String> currIdList = new ArrayList<>();
-        dfs(user_id, banned_id, visited,currIdList);
+        resultSet = new HashSet<>();
+        used = new boolean[user_id.length];
+        List<String> currMatchIdList = new ArrayList<>();
+        dfs(user_id,banned_id,used,currMatchIdList);
         answer = resultSet.size();
         return answer;
     }
-    static void dfs(String[] user_id, String[] banned_id, boolean[] visited,
-                    List<String> currIdList){
-        int currIdSize = currIdList.size();
-        if(currIdList.size() == banned_id.length){
-            Collections.sort(currIdList); //정렬해서 set에 넣어야지 중복제거 가능
-            resultSet.add(currIdList); //배열이 아닌 List로 넣어야지 set에서 중복제거가됨
+    static void dfs(String[] user_id, String[] banned_id, boolean[] visited, 
+                    List<String> currMatchIdList){
+        int currLen = currMatchIdList.size();
+        if(currLen==banned_id.length){//현재 제제아이디 목록 길이와 ban해야할 id길이가 같다면 조합이 완성된것
+            List<String> result = new ArrayList<>(currMatchIdList);
+            Collections.sort(result); // 정렬을 해야 set 비교가 된다
+            resultSet.add(result);//결과 set에다가 제제 아이디 조합 추가
             return;
         }
-       String banId = banned_id[currIdSize];//현재 금지된 아이디 몇번째까지 탐색한건지
-       for(int i=0;i<user_id.length;i++){
-           if(isMatch(user_id[i],banId)){//userId랑 banId랑 매치가 되는지 여부 확인
-               if(!visited[i]){//이미 사용된 아이디가 아니라면
-                   visited[i] = true;
-                   currIdList.add(user_id[i]);
-                   dfs(user_id,banned_id,visited,currIdList);
-                   visited[i] = false;
-                   currIdList.remove(user_id[i]);
-               }
-           }
-       }
+        String banId = banned_id[currLen];//이번에 탐색할 banId
         
+        for(int i=0; i<user_id.length;i++){
+            if(!visited[i] && isMatch(user_id[i], banId)){
+                visited[i] = true;
+                currMatchIdList.add(user_id[i]);
+                // System.out.println("추가되는 user_id  :" + user_id[i] + currMatchIdList.toString());
+                dfs(user_id, banned_id, visited, currMatchIdList);
+                visited[i] = false;
+                currMatchIdList.remove(currMatchIdList.size()-1);
+                // System.out.println("제거되는 user_id :" + u-ser_id[i] + " "+currMatchIdList.toString());
+            }
+        }
     }
+    
     private static boolean isMatch(String userId, String banId){
         if(userId.length() != banId.length()){
             return false;
         }
-        for(int i=0;i<userId.length();i++){
-            char userChar = userId.charAt(i);
-            char banChar = banId.charAt(i);
-            
-            if(banChar == '*') continue;
-            
-            if(userChar==banChar)continue;
-            else{
-                return false;
-            }
+        for(int i=0; i<userId.length();i++){
+            if(banId.charAt(i) == '*') continue;
+            if(userId.charAt(i) != banId.charAt(i)) return false;
         }
         return true;
     }
 }
-//불량 사용자 -> 당첨 처리 시 제외
-//아이디 마스킹
-// 1. 문자 하나당 *한개 사용
-// 2. 아이디 당 최소 하나 이상의 * 사용
-// 당첨에서 제제 아이디 목록은 몇가지 경우수가 가능한지 return
-// 매칭이 되려면 ,
-// 1. 문자열 길이 같아야함, 2.모든 부분문자열 포함해야함
-
-//아이디 목록이 8개밖에안됨(길이도 8 이하)
+//불량 사용자 : 당첨 처리 시 제외
+// 문자에 마스킹 : 1. 문자 하나당 * 하나 사용 + 2.아이디 하나 당 최소 하나의 * 사용함
+//제제 아아디 목록의 경우의 수를 구하시오
+//user_id배열 크기 8 , 문자열 길이 8이하 -> 크기가 너무 작다 완탐? ( 아이디 중복도 없음)
+// 중복 아이디 찾고 Set에 저장하면 알아서 중복된 조합은 제거 될듯?
