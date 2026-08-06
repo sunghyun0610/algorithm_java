619b89bb7c118eb92ee6c20ec99133fddfb9378b
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-08-06 14:43:16 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 13.17 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public long solution(int[] sequence) {
+        long answer = 0;
+        int len = sequence.length;
+        int [] arr1 = new int[len]; //펄스 +1로시작하는거
+        int [] arr2 = new int[len]; // 펄스 -1로 시작하는거
+
+        for(int i=0;i<len;i++){
+            if(i%2==0){
+                arr1[i] =sequence[i];
+                arr2[i] = -sequence[i];
+            }
+            else{
+                arr1[i] = -sequence[i];
+                arr2[i] = sequence[i];
+            }
+        }
+        //배열에서 최대 연속 합문제로 바뀜 Kaden
+    long curr1=arr1[0];
+        long max1 = arr1[0];
+       for(int i=1;i<len;i++){
+           curr1 = Math.max(arr1[i],curr1+arr1[i]);
+           max1 = Math.max(max1,curr1);
+       }
+           long curr2=arr2[0];
+        long max2 = arr2[0];
+       for(int i=1;i<len;i++){
+           curr2 = Math.max(arr2[i], curr2+arr2[i]);
+           //System.out.println(curr2);
+           max2 = Math.max(curr2,max2);
+       }
+        answer = Math.max(max2,max1);
+        return answer;
+    }
+}
