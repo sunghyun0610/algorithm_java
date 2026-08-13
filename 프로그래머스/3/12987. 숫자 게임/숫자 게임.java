cb71acd6e3e6f2f3464598feb731ab3ee79bc569
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-08-13 14:01:28 +0900

    [level 3] Title: 숫자 게임, Time: 39.06 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,38 +1,28 @@
 import java.util.*;
 class Solution {
-    public int solution(int [] A, int[] B) {
+    public int solution(int[] A, int[] B) {
         int answer = 0;
+        Arrays.sort(A);
         Arrays.sort(B);
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-
-        int len = B.length;
-        for(int i=0;i<len;i++){
-            int key = B[i];
+        int left = 0;
+        int right = A.length-1;
+        int BIdx=0;
+        int AIdx=0;
+        
+        while(AIdx < A.length && BIdx<A.length){
+            int num = A[AIdx];
+               if(num>=B[BIdx]){
+                   BIdx++;
+               } else{
+                   BIdx++;
+                   AIdx++;
+                   answer++;
+               }
             
-            
-            if(map.get(key)!=null){
-                int value = map.get(key);
-                map.put(key,value+1);
-            }
-            else{
-                map.put(key,1);
-            }
-
         }
-        for(int i=0;i<len;i++){
-            int a = A[i];
-            Integer result = map.higherKey(a);
-            if(result == null) continue;
-            int value = map.get(result);
-            if(value >0){
-                answer++;
-                map.put(result,value-1);
-                if(value-1 ==0){
-                    map.remove(result);
-                }
-                
-            }
-        }
+        
         return answer;
     }
 }
+//A를 정렬한다
+//차이가 제일 작은 원소를 선택하여 승부한다
