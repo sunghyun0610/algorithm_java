69cc6fc088e126ae0b86bc7d986c9ad3cf7fc590
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-08-15 18:36:56 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 4.60 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,82 +1,69 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Solution {
-    static class Node {
+    static int[] dx = {1,-1,0,0};//아래로
+    static int[] dy = {0,0,-1,1};//오른쪽으로
+    
+    static class Node{
         int x;
         int y;
-        int dir;//방향
-        int cost;//값
-        
-        Node(int x,int y,int dir, int cost){
-            this.x=x;
-            this.y=y;
-            this.dir = dir;
+        int dis;//방향 위쪽(0)인지 오른쪽(1)인지
+        int cost;
+        Node(int x, int y, int dis, int cost){
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
             this.cost = cost;
         }
     }
-    
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static boolean[][] visited;
-    static int minMoney;
     public int solution(int[][] board) {
         int answer = 0;
         int row = board.length;
-        minMoney = Integer.MAX_VALUE;
-        visited = new boolean[row][row];
-        Node startNode = new Node(0,0,0,0);
-        bfs(startNode,row-1,row-1,board);
-        answer=minMoney;
-        return answer;
-    }
-    static void bfs(Node startNode, int endX, int endY,int[][] board){
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(startNode);
-        int[][][] cost = new int[endX+1][endX+1][2];
-        for(int i=0;i<=endX;i++){
-            for(int j=0;j<=endX;j++){
-                cost[i][j][0] = Integer.MAX_VALUE;
-                cost[i][j][1] = Integer.MAX_VALUE;
+        int[][][] dpMap = new int[row][row][2];//지금까지 최소 비용을 기록해 놓는 배열
+        for(int[][] k : dpMap){
+            for(int[] l :k){
+                l[0] = Integer.MAX_VALUE;
+                l[1] = Integer.MAX_VALUE;
             }
         }
+        bfs(board,dpMap,row-1,row-1);
+        return Math.min(dpMap[row-1][row-1][0],dpMap[row-1][row-1][1]);
+    }
+    static void bfs(int[][] board, int[][][] dpMap, int endX, int endY){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0,0,0,0));
         while(!queue.isEmpty()){
             Node currNode = queue.poll();
             int currX = currNode.x;
             int currY = currNode.y;
-            int currDir = currNode.dir;
+            int currDis = currNode.dis;
             int currCost = currNode.cost;
-            if(currX==endX && currY == endY){
-                minMoney = Math.min(minMoney, currCost);
-            }
-            
-            for(int i=0;i<4;i++){
+            for(int i=0;i<4;i++){ //첫번째 칸인 경우
                 int nextX = currX + dx[i];
-                int nextY = currY + dy[i];
-                int nextDir = i<2 ? 1 : 0;
-                int nextCost =0;
-                if(currDir == nextDir){
-                    nextCost = currCost + 100;
-                }
-                else{
-                    nextCost = currCost + 600;
-                }
-                if(nextX>=0 && nextX<=endX && nextY>=0 && nextY<=endY &&
-                  board[nextX][nextY]==0 && nextCost < cost[nextX][nextY][nextDir]){
-                    if(currX == 0 && currY == 0){//처음인 경우 방향고려 X
-                        cost[nextX][nextY][nextDir]= currCost+100;
-                        queue.add(new Node(nextX, nextY, nextDir, currCost+100));
-                    }else{
-                        cost[nextX][nextY][nextDir]= nextCost;
-                        queue.add(new Node(nextX, nextY, nextDir, nextCost));
-                    }
+                int nextY = currY +dy[i];
+                int nextDis = i<2 ? 1:0; // 1이 상하방향, 0 이 좌우방향을 뜻함
+                int nextCost = 0;
+                if(nextDis==currDis) nextCost = currCost +100;
+                else nextCost = currCost + 600;
+                if(nextX>=0 && nextX<=endX && nextY>=0 && nextY <=endY
+                  &&board[nextX][nextY]==0 && dpMap[nextX][nextY][nextDis]>nextCost){
                     
+                     if(currX == 0 && currY == 0){
+                       dpMap[nextX][nextY][nextDis] = 100;
+                       queue.add(new Node(nextX,nextY,nextDis,100));
+                    }
+                    else{//첫번 째 칸 이외의 경우
+                        dpMap[nextX][nextY][nextDis] = nextCost;
+                        queue.add(new Node(nextX,nextY,nextDis,nextCost));
+                        
+                    }
                 }
+               
             }
         }
-        
     }
 }
-//전체탐색 -> dfs + 백트래킹(최소 비용보다 비용 커지면 return)
-//방향 꺽을때마다 코너 필요함
-// way : 1 상하로 / way : 0 좌우로
+//경주로를 건설하는데 필요한 최소 비용
+//25*25시 dfs로 모든 경로 탐색은 비효율 -> bfs로 가야한다
+//만약 경주로 건설이 다 1씩 비용이라면 visited으로 처리가능
+//but 코너 변수로 한번에 1쓸지, 6쓸지 매번 고려해야함
