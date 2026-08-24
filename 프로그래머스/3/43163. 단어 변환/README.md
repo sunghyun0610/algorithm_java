328a14d7f15bf42075afe980ec2e969e75235cca
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-08-24 21:16:27 +0900

    [level 3] Title: 단어 변환, Time: 8.14 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.94 ms
+메모리: 89.2 MB, 시간: 8.14 ms
 
 ### 구분
 
@@ -16,57 +16,57 @@
 
 ### 제출 일자
 
-2026년 07월 21일 14:10:19
+2026년 08월 24일 21:16:23
 
 ### 문제 설명
 
-<p>두 개의 단어 begin, target과 단어의 집합 words가 있습니다. 아래와 같은 규칙을 이용하여 begin에서 target으로 변환하는 가장 짧은 변환 과정을 찾으려고 합니다.</p>
-<div class="highlight"><pre class="codehilite"><code>1. 한 번에 한 개의 알파벳만 바꿀 수 있습니다.
+<p style="user-select: auto !important;">두 개의 단어 begin, target과 단어의 집합 words가 있습니다. 아래와 같은 규칙을 이용하여 begin에서 target으로 변환하는 가장 짧은 변환 과정을 찾으려고 합니다.</p>
+<div class="highlight" style="user-select: auto !important;"><pre class="codehilite" style="user-select: auto !important;"><code style="user-select: auto !important;">1. 한 번에 한 개의 알파벳만 바꿀 수 있습니다.
 2. words에 있는 단어로만 변환할 수 있습니다.
 </code></pre></div>
-<p>예를 들어 begin이 "hit", target가 "cog", words가 ["hot","dot","dog","lot","log","cog"]라면 "hit" -&gt; "hot" -&gt; "dot" -&gt; "dog" -&gt; "cog"와 같이 4단계를 거쳐 변환할 수 있습니다.</p>
+<p style="user-select: auto !important;">예를 들어 begin이 "hit", target가 "cog", words가 ["hot","dot","dog","lot","log","cog"]라면 "hit" -&gt; "hot" -&gt; "dot" -&gt; "dog" -&gt; "cog"와 같이 4단계를 거쳐 변환할 수 있습니다.</p>
 
-<p>두 개의 단어 begin, target과 단어의 집합 words가 매개변수로 주어질 때, 최소 몇 단계의 과정을 거쳐 begin을 target으로 변환할 수 있는지 return 하도록 solution 함수를 작성해주세요.</p>
+<p style="user-select: auto !important;">두 개의 단어 begin, target과 단어의 집합 words가 매개변수로 주어질 때, 최소 몇 단계의 과정을 거쳐 begin을 target으로 변환할 수 있는지 return 하도록 solution 함수를 작성해주세요.</p>
 
-<h5>제한사항</h5>
+<h5 style="user-select: auto !important;">제한사항</h5>
 
-<ul>
-<li>각 단어는 알파벳 소문자로만 이루어져 있습니다.</li>
-<li>각 단어의 길이는 3 이상 10 이하이며 모든 단어의 길이는 같습니다.</li>
-<li>words에는 3개 이상 50개 이하의 단어가 있으며 중복되는 단어는 없습니다.</li>
-<li>begin과 target은 같지 않습니다.</li>
-<li>변환할 수 없는 경우에는 0를 return 합니다.</li>
+<ul style="user-select: auto !important;">
+<li style="user-select: auto !important;">각 단어는 알파벳 소문자로만 이루어져 있습니다.</li>
+<li style="user-select: auto !important;">각 단어의 길이는 3 이상 10 이하이며 모든 단어의 길이는 같습니다.</li>
+<li style="user-select: auto !important;">words에는 3개 이상 50개 이하의 단어가 있으며 중복되는 단어는 없습니다.</li>
+<li style="user-select: auto !important;">begin과 target은 같지 않습니다.</li>
+<li style="user-select: auto !important;">변환할 수 없는 경우에는 0를 return 합니다.</li>
 </ul>
 
-<h5>입출력 예</h5>
-<table class="table">
-        <thead><tr>
-<th>begin</th>
-<th>target</th>
-<th>words</th>
-<th>return</th>
+<h5 style="user-select: auto !important;">입출력 예</h5>
+<table class="table" style="user-select: auto !important;">
+        <thead style="user-select: auto !important;"><tr style="user-select: auto !important;">
+<th style="user-select: auto !important;">begin</th>
+<th style="user-select: auto !important;">target</th>
+<th style="user-select: auto !important;">words</th>
+<th style="user-select: auto !important;">return</th>
 </tr>
 </thead>
-        <tbody><tr>
-<td>"hit"</td>
-<td>"cog"</td>
-<td>["hot", "dot", "dog", "lot", "log", "cog"]</td>
-<td>4</td>
+        <tbody style="user-select: auto !important;"><tr style="user-select: auto !important;">
+<td style="user-select: auto !important;">"hit"</td>
+<td style="user-select: auto !important;">"cog"</td>
+<td style="user-select: auto !important;">["hot", "dot", "dog", "lot", "log", "cog"]</td>
+<td style="user-select: auto !important;">4</td>
 </tr>
-<tr>
-<td>"hit"</td>
-<td>"cog"</td>
-<td>["hot", "dot", "dog", "lot", "log"]</td>
-<td>0</td>
+<tr style="user-select: auto !important;">
+<td style="user-select: auto !important;">"hit"</td>
+<td style="user-select: auto !important;">"cog"</td>
+<td style="user-select: auto !important;">["hot", "dot", "dog", "lot", "log"]</td>
+<td style="user-select: auto !important;">0</td>
 </tr>
 </tbody>
       </table>
-<h5>입출력 예 설명</h5>
+<h5 style="user-select: auto !important;">입출력 예 설명</h5>
 
-<p>예제 #1<br>
+<p style="user-select: auto !important;">예제 #1<br style="user-select: auto !important;">
 문제에 나온 예와 같습니다.</p>
 
-<p>예제 #2<br>
+<p style="user-select: auto !important;">예제 #2<br style="user-select: auto !important;">
 target인 "cog"는 words 안에 없기 때문에 변환할 수 없습니다.</p>
 
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,61 +1,64 @@
 import java.util.*;
+
 class Solution {
-    static Queue<String> queue  = new LinkedList<>();
-    static boolean[] visited;
+    static class Info{
+        String letter;//지금 문자열이 뭔지
+        int cnt;//몇번 탐색했는지
+        Info(String letter, int cnt){
+            this.letter = letter;
+            this.cnt=cnt;
+        }
+    }
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        int wordsLen = words.length;
-        int wordLen = target.length();// 모든 단어의 길이가 같다
-        visited = new boolean[wordsLen];
-        boolean isHasTarget = false;
-        for(int i=0;i<wordsLen;i++){
-            if(words[i].equals(target)){
-                isHasTarget=true;
-            }
-        }
-        if(!isHasTarget) return 0;
-        answer = bfs(begin,target, words, wordLen);
-        return answer;
+        int wordLen = words.length;
+        boolean [] visited = new boolean[wordLen];
+        return bfs(begin,target,words,visited);
+        
+       
     }
-    public static int bfs(String word, String target, String[] words, int wordLen){
-        Queue<Integer> countQueue = new LinkedList<>();
-        countQueue.add(0);
-        int len = words.length;
-        queue.add(word);
+    static int bfs(String begin, String target, String[] words, boolean[] visited){
+        Queue<Info> queue = new LinkedList<>();
+        queue.add(new Info(begin,0));
+        int targetLen = target.length();//이게 문자열 길이임
         while(!queue.isEmpty()){
-           //System.out.println("cnt : "+cnt);
-        String currWord = queue.poll(); //현재 문자열
-        int cnt = countQueue.poll(); // 현재 변환 횟수변수
-        if(cnt>=len) return 0;
-        for(int i=0;i<words.length;i++){ //전체 문자 배열에서 탐색한다
-            char diffChar=' ';
-            int diffIndex = 0;
-            
-            for(int j=0;j<wordLen;j++){ // 해당 문자열과 비교 문자열 2개 비교
-                if(currWord.charAt(j)!= words[i].charAt(j)){
-                    diffChar = currWord.charAt(j);
-                    diffIndex = j;
-                    //System.out.println("diffChar : "+diffChar + "diffIndex : "+diffIndex);
-                    break;
-                    
-                }
-            }
-            if(diffChar!=' ' && diffIndex>=0){
-                if((currWord.substring(0,diffIndex) + currWord.substring(diffIndex+1)).equals(words[i].substring(0,diffIndex)+words[i].substring(diffIndex+1)) && !visited[i]){
-                    // System.out.println("String 1 : "+(currWord.substring(0,diffIndex) + currWord.substring(diffIndex+1) + "String2 : "+ words[i].substring(0,diffIndex)+words[i].substring(diffIndex+1)));
-                    queue.add(words[i]);
-                    visited[i]=true;
-                    countQueue.add(cnt+1);
-                    //System.out.println("현재 문자열 : "+ currWord + " 다음 문자열 : "+words[i]);
-            if(words[i].equals(target)){
-            return cnt+1;
-        }
+            Info currInfo = queue.poll();
+            String currWord = currInfo.letter;
+            int currCnt = currInfo.cnt;
+            List<String> linkStr = new ArrayList<>();
+            if(currWord.equals(target)){
+                // System.out.println("끝 "+ currCnt);
+                return currCnt;
+                
+            } 
+            for(int i=0;i<words.length;i++){
+                if(!visited[i] && isLinkedWord(currWord, words[i])){
+                        // System.out.println("들어옴?" + words[i]);
+                        linkStr.add(words[i]);
+                        visited[i] = true;
                     }
-                }
             }
+            for(int i=0; i<linkStr.size();i++){
+                queue.add(new Info(linkStr.get(i),currCnt+1));
+            }
+            
+            
         }
-            return 0;
+        return 0;
+    }
+    
+    static boolean isLinkedWord(String str1, String str2){
+        int len = str1.length();
+        for(int i=0;i<len;i++){
+            String front1 = str1.substring(0,i);
+            String back1 = str1.substring(i+1);
+            String front2 = str2.substring(0,i);
+            String back2 = str2.substring(i+1);
+            if(front1.equals(front2) && back1.equals(back2)) return true;
+        }
+        return false;
     }
 }
-//한글자만 바뀐지 체크는 substring으로 앞뒤 같은지 체크해보자.
-// 그래프에서 한글자만 바뀐 것끼리만 연결되어있는 것임.
+//begin -> target 변환하는 가장 짧은 변환과정 / 변환 불가하면 0 return
+//변환 규칙 1.한번에 한개의 알파벳만 바꾼다
+// 규칙 2. words에 있는 단어로만 변환 가능하다 (중복된 단어는 없다)
