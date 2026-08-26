18063a0b44b894b56b5b3f49ee515aa351772cad
sunghyun <101376904+sunghyun0610@users.noreply.github.com>
2026-08-26 10:57:05 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 26.41 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,49 +1,45 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int answer = Integer.MAX_VALUE;
-        long solveTime = 0;//걸리는 총 시간
-        //무조건 통과하는 숙련도 부터 줄여나가야겠네.
-        //정렬은 안됨.
-        int maxLevel = 0; //무조건 통과하는 레벨임
+        int answer = 0;
+        int maxLevel=0;
         for(int i=0;i<diffs.length;i++){
             maxLevel = Math.max(maxLevel,diffs[i]);
         }
         int minLevel = 1;
-        int midLevel = 0;
+        int mid = (minLevel+maxLevel)/2;
         while(minLevel<=maxLevel){
-            solveTime = times[0];//초기화
-            midLevel = (maxLevel+minLevel)/2;
-
+            mid = (minLevel+maxLevel)/2;
+            long sum=times[0];
             for(int i=1;i<diffs.length;i++){
-                solveTime+= getSolveTime(diffs[i],midLevel,times[i-1],times[i]);
+                sum+=completeTime(diffs[i],mid,times[i],times[i-1]);
             }
-
-            if(solveTime>limit){
-                minLevel = midLevel+1;
-                
+            if(sum>limit){//level올려야함
+                minLevel = mid+1;
+            }else if(sum==limit){
+                answer = mid;
+                break;
             }
-            else if(solveTime<=limit){
-                maxLevel=midLevel-1;
-                answer = Math.min(answer,midLevel);
-            }
-
+            else{ // level 내려도됨
+                maxLevel = mid-1;
+                answer = mid;            }
         }
+        
         return answer;
     }
-    private static long getSolveTime(int diff, int level, int prevTime, int currTime){
-        long sum=0;
+    private long completeTime(int diff, int level, int currTime, int prevTime){
         if(diff<=level) return currTime;
         else{
-            int wrongTime = diff-level;
-            sum = (long)((currTime+prevTime) * wrongTime)+currTime;
+            int sum=0;
+            int retryCnt = diff-level;
+            sum = (currTime + prevTime) * retryCnt + currTime;
+            return sum;
         }
-        return sum;
-    }//퍼즐 1개푸는데 시간 구하기
+    }
 }
-//n개의 퍼즐 시간 내에 풀어야함
-// 퍼즐 = 난이도 / 소요시간 정해져있음
-// 내 숙련도에 따라 퍼즐을 풀 때 틀리는 횟수가 바뀜
-// 퍼즐 난이도 diffs, 소요시간 times , 제한시간 limit
-//퍼즐 틀리는경우( diff >level) time_cur만큼 시간 사용 + time+prev만큼 시간 사용해서 이전퍼즐 풀고와야함(실패는 안함)
-//제한시간내에 퍼즐 모두 해결하기 휘안 숙련도의 최솟값 필요함
-//이분탐색으로 갈까
+//현재 퍼즐 난이도 diff, 나의 숙련도 level
+//diff <=level 이면 time만큼 시간써서 해결
+//diff > level이면, diff-level만큼 틀리고,
+//  (틀릴때마다 time_cur만큼 사용 + 이전문제 풀고와야함) * 틀린횟수 + time_curr
+//제한 시간내에 모두 해결하기 위한 level 최솟값
